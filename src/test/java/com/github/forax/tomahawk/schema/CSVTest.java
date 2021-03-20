2bb0ad4410d4fa461e98a5590a07551c31ac20e4
forax <forax@univ-mlv.fr>
2021-03-20 13:00:53 +0100

    add a test of Table.load()

--- src/test/java/com/github/forax/tomahawk/schema/CSVTest.java
+++ src/test/java/com/github/forax/tomahawk/schema/CSVTest.java
@@ -1,10 +1,18 @@
 package com.github.forax.tomahawk.schema;
 
+import com.github.forax.tomahawk.schema.Layout.StructLayout;
+import com.github.forax.tomahawk.vec.ListVec;
+import com.github.forax.tomahawk.vec.StructVec;
+import com.github.forax.tomahawk.vec.U16Vec;
 import org.junit.jupiter.api.Test;
 
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 import static com.github.forax.tomahawk.schema.Layout.double64;
 import static com.github.forax.tomahawk.schema.Layout.field;
@@ -13,7 +21,9 @@ import static com.github.forax.tomahawk.schema.Layout.string;
 import static com.github.forax.tomahawk.schema.Layout.struct;
 import static java.nio.file.Files.createTempDirectory;
 import static java.nio.file.Files.delete;
+import static java.nio.file.Files.getLastModifiedTime;
 import static java.nio.file.Files.list;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CSVTest {
   @Test
@@ -27,11 +37,11 @@ public class CSVTest {
         air, moon roof, loaded",4799.00
         """;
     var layout = struct(false,
-        field("Year", int32(false)),
-        field("Make", string(false)),
-        field("Model", string(false)),
+        field("Year",        int32(false)),
+        field("Make",        string(false)),
+        field("Model",       string(false)),
         field("Description", string(true)),
-        field("Price", double64(true))
+        field("Price",       double64(true))
     );
     var directory = createTempDirectory("data");
     Closeable defer = () -> {
@@ -42,6 +52,43 @@ public class CSVTest {
     };
     try(defer) {
       CSV.fetch(csv, layout, directory, "cars");
+    }
+  }
+
+  @Test
+  public void fetchAndLoad() throws IOException {
+    var csv = """
+        Name,Job Title,Address,State,City
+        "Doe, John",Designer,325 Pine Street,,Seattle
+        "Green, Edward",Developer,110 Pike Street,WA,Seattle
+        """;
+    var layout = Layout.parse("""
+        struct(false,
+            field("Name",      string(false)),
+            field("Job Title", string(false)),
+            field("Address",   string(false)),
+            field("State",     string(true)),
+            field("City",      string(false))
+        )
+        """);
+    var directory = createTempDirectory("jobs");
+    Closeable defer = () -> {
+      for (var temp : list(directory).toList()) {
+        delete(temp);
+      }
+      delete(directory);
+    };
+    try(defer) {
+      CSV.fetch(csv, (StructLayout) layout, directory, "jobs");
+      var vec = (StructVec) Table.map(directory, "jobs");
+      var name = (ListVec<?>) vec.fields().get(0);
+      var state = (ListVec<?>) vec.fields().get(3);
+
+      var names = LongStream.range(0, name.length()).mapToObj(name::getString).toList();
+      assertEquals(List.of("Doe, John", "Green, Edward"), names);
+
+      var states = LongStream.range(0, name.length()).mapToObj(state::getString).toList();
+      assertEquals(Arrays.asList(null, "WA"), states);
     }
   }
 }
