c2e1e7ea5b64445a031506d7b9669c6b82587a78
forax <forax@univ-mlv.fr>
2021-03-25 20:22:40 +0100

    add more perf test

--- src/main/java/com/github/forax/tomahawk/perf/VecOpPerfTest.java
+++ src/main/java/com/github/forax/tomahawk/perf/VecOpPerfTest.java
@@ -17,6 +17,8 @@ import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.annotations.Warmup;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
@@ -36,15 +38,50 @@ public class VecOpPerfTest {
   private Path destPath, path1, path2;
   private Vec dest, v1, v2;
 
+  private static final Vec DEST, V1, V2;
+  private static final ByteBuffer DEST_BUFFER, V1_BUFFER, V2_BUFFER;
+  static {
+    Path destPath, path1, path2;
+    try {
+      destPath = Files.createTempFile("vec-op-benchmark", "");
+      path1 = Files.createTempFile("vec-op-benchmark", "");
+      path2 = Files.createTempFile("vec-op-benchmark", "");
+      DEST = U32Vec.mapNew(null, path1, 100_000);
+      V1 = U32Vec.mapNew(null, destPath, 100_000);
+      IntStream.range(0, (int) V1.length()).forEach(i -> ((U32Vec) V1).setInt(i, i));
+      V2 = U32Vec.mapNew(null, path2, 100_000);
+      IntStream.range(0, (int) V2.length()).forEach(i -> ((U32Vec) V2).setInt(i, i));
+
+      DEST_BUFFER = VecOpsPerf.asByteBuffer((U32Vec) DEST);
+      V1_BUFFER = VecOpsPerf.asByteBuffer((U32Vec) V1);
+      V2_BUFFER = VecOpsPerf.asByteBuffer((U32Vec) V2);
+
+    } catch(IOException e) {
+      throw new UncheckedIOException(e);
+    }
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      try {
+        V2.close();
+        V1.close();
+        DEST.close();
+        Files.delete(path2);
+        Files.delete(path1);
+        Files.delete(destPath);
+      } catch(IOException e) {
+        throw new UncheckedIOException(e);
+      }
+    }));
+  }
+
   @Setup
   public void setup() throws IOException {
     destPath = Files.createTempFile("vec-op-benchmark", "");
     path1 = Files.createTempFile("vec-op-benchmark", "");
     path2 = Files.createTempFile("vec-op-benchmark", "");
     dest = U32Vec.mapNew(null, destPath, 100_000);
-    v1 = U32Vec.mapNew(null, destPath, 100_000);
+    v1 = U32Vec.mapNew(null, path1, 100_000);
     IntStream.range(0, (int) v1.length()).forEach(i -> ((U32Vec) v1).setInt(i, i));
-    v2 = U32Vec.mapNew(null, destPath, 100_000);
+    v2 = U32Vec.mapNew(null, path2, 100_000);
     IntStream.range(0, (int) v2.length()).forEach(i -> ((U32Vec) v2).setInt(i, i));
   }
 
@@ -71,6 +108,12 @@ public class VecOpPerfTest {
   }
 
   @Benchmark
+  public Vec op_add_hand_crafted_hard_coded() {
+    VecOpsPerf.handCraftedHardCodedInt(DEST_BUFFER, V1_BUFFER, V2_BUFFER, (a, b) -> a + b);
+    return DEST;
+  }
+
+  /*@Benchmark
   public Vec op_add_vec_loop() {
     var implDest = (U32Vec) dest;
     var implV1 = (U32Vec) v1;
@@ -79,5 +122,5 @@ public class VecOpPerfTest {
       implDest.setInt(i, implV1.getInt(i) + implV2.getInt(i));
     }
     return dest;
-  }
+  }*/
 }
--- src/main/java/com/github/forax/tomahawk/vec/VecOpsPerf.java
+++ src/main/java/com/github/forax/tomahawk/vec/VecOpsPerf.java
@@ -4,6 +4,7 @@ package com.github.forax.tomahawk.vec;
 import jdk.incubator.vector.IntVector;
 import jdk.incubator.vector.VectorSpecies;
 
+import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 public class VecOpsPerf {
@@ -14,6 +15,57 @@ public class VecOpsPerf {
     var buffer1 = ((VecImpl.U32Impl) vec1).dataSegment().asByteBuffer();
     var buffer2 = ((VecImpl.U32Impl) vec2).dataSegment().asByteBuffer();
 
+    if (buffer1.capacity() != buffer2.capacity() ||
+        buffer1.capacity() != bufferDest.capacity()) {
+      throw new IllegalArgumentException("not the same length");
+    }
+
+    var sliceLength = SPECIES_U32.vectorByteSize();
+
+    //System.err.println("slice1 " + slice1 + " slice2 " + slice2);
+
+    var loopBound = bufferDest.capacity() - sliceLength;
+
+    //System.err.println("i1LoopBound " + i1LoopBound + " i2LoopBound " + i2LoopBound);
+
+    // main loop
+    var i = 0;
+    while (i < loopBound ) {
+      var v1 = IntVector.fromByteBuffer(SPECIES_U32, buffer1, i, ByteOrder.LITTLE_ENDIAN);
+      var v2 = IntVector.fromByteBuffer(SPECIES_U32, buffer2, i, ByteOrder.LITTLE_ENDIAN);
+
+      //System.err.println("load v1 " + v1);
+      //System.err.println("load v2 " + v2);
+
+      var result = v1.add(v2);
+
+      //System.err.println("result " + result);
+
+      result.intoByteBuffer(bufferDest, i, ByteOrder.LITTLE_ENDIAN);
+
+      i += sliceLength;
+    }
+
+    // post loop
+    while (i < bufferDest.capacity()) {
+      var v1 = buffer1.getInt(i);
+      var v2 = buffer2.getInt(i);
+
+      //System.err.println("plain load v1 " + v1);
+      //System.err.println("plain load v2 " + v2);
+
+      var result = v1 + v2;
+      bufferDest.putInt(i, result);
+
+      i += 4;
+    }
+  }
+
+  public static ByteBuffer asByteBuffer(U32Vec vec) {
+    return ((VecImpl.U32Impl) vec).dataSegment().asByteBuffer();
+  }
+
+  public static void handCraftedHardCodedInt(ByteBuffer bufferDest, ByteBuffer buffer1, ByteBuffer buffer2, VecOp.IntBinOp binaryOp) {
     if (buffer1.capacity() != buffer2.capacity() ||
         buffer1.capacity() != bufferDest.capacity()) {
       throw new IllegalArgumentException("not the same length");
